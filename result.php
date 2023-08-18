956d3d48305c585d2423cd33ca8062db0a2e2f7d
そうくん <daoyecangma@gmail.com>
2023-08-19 07:15:10 +0900

    直しテスト

--- result.php
+++ result.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <title>Love Language</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <?php echo $_POST["Q1"]; ?>
-    </body>
-</html>
+<?php
+    echo $_POST["Q1"];
+?>
