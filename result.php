f9c8f305b3a0500b6abec3ca6b7e796f06410b81
そうくん <daoyecangma@gmail.com>
2023-08-21 13:58:36 +0900

    いけたかも

--- result.php
+++ result.php
@@ -1,3 +1,15 @@
-<?php
-    echo $_POST["Q1"];
-?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Love Language(結果)</title>
+    </head>
+    <body>
+        <p>
+            あなたが選んだのは
+            <?php echo htmlspecialchars(@$_POST['Q1'], ENT_QUOTES, 'UTF-8'); ?>
+            です。
+        </p>
+        <!--<p>あなたは、<?php echo (int)@$_POST['age']; ?> 歳です。</p>-->
+    </body>
+</html>
